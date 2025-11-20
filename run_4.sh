fe53224a6e27bbb855122f36c396105cdae91468
borker <mauryk.mauryk@gmail.com>
2025-11-20 01:44:26 +0100

    new

--- run_4.sh
+++ run_4.sh
@@ -1,4 +1,4 @@
-./run.sh --processing-date=2019-10-06 --vehicle-type yellow --boroughs Manhattan --methods LP MinMaxCostFlow --start-hour 00 --end-hour 23 --time-delta 15 --num-iter 99 --log-level INFO --num-workers 44 --time-window-size 30 --lp-price-grid-size 70
+./run.sh --processing-date=2019-10-06 --vehicle-type yellow --boroughs Manhattan --methods LP MinMaxCostFlow --start-hour 00 --end-hour 23 --time-delta 15 --num-iter 99 --log-level INFO --num-workers 33 --time-window-size 30 --lp-price-grid-size 70
 
 ./run.sh --processing-date=2019-10-08 --vehicle-type yellow --boroughs Manhattan --methods LP MinMaxCostFlow --start-hour 00 --end-hour 23 --time-delta 15 --num-iter 99 --log-level INFO --num-workers 33 --time-window-size 30 --lp-price-grid-size 70
 
